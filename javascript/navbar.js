621f0df75a1e4b817ccd0f37f4abdae0bbfedc93
Rahul Rupchand Jidge <rahul.jidge@gmail.com>
2022-07-19 10:03:24 +0530

    first-commit

---
+++ javascript/navbar.js
@@ -0,0 +1,9 @@
+
+function show(){
+  var x=document.getElementById("humberger-div");
+  x.style.display="block"
+}
+function closeNav(){
+  var y=document.getElementById("humberger-div");
+  y.style.display="none"
+}
